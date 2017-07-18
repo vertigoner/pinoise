1877cad92ce0b577564785d074355d0c74ca5ff1
Noah <noah.roberts@codemettle.com>
2017-07-18 16:15:55 -0400

    added buttons and leds

--- pinoise/pinoise.py
+++ pinoise/pinoise.py
@@ -1,6 +1,22 @@
 import pygame
+from gpiozero import Button, LED
 
 pygame.init()
+
+
+buttons = {
+           "BTN1": Button(17)
+           "BTN2": Button(27)
+           "BTN3": Button(22)
+           "BTN4": Button(10)
+           }
+
+leds = {
+        "LED1": LED(5)
+        "LED2": LED(6)
+        "LED3": LED(13)
+        "LED4": LED(19)
+        }
 
 # testing portion
 my_sound = pygame.mixer.Sound("path/mysound.wav")
